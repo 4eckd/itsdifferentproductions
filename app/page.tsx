0d82409b99596b0d2fd6a88567b887b276d17eb3
jlucus <jlucus916@gmail.com>
2025-04-13 10:36:30 -0700

    .

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Globe } from "@/components/globe"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 relative">
+      <div className="relative w-full max-w-[480px] aspect-square">
+        <Globe />
+        <h1 className="mt-1 text-gray-800 flex min-h-screen flex-col items-center justify-center p-2 relative">Its Different Productions ™</h1>
+        <p className="mt-1 text-gray-800 flex items-center justify-center">Welcome to Our new Digital Shop across the Globe!
+        </p>
+      </div>
+    </main>
+  )
+}
